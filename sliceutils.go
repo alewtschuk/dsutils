f438ba9f56d68c4b7ff9ab71dc2a26c88ae07781
Alex <aptik.dev@gmail.com>
2025-02-20 23:17:07 -0700

    Implemented EnsureUnique

--- sliceutils.go
+++ sliceutils.go
@@ -15,3 +15,25 @@ func Remove[Element comparable](elementToDelete Element, slice []Element) []Elem
 	}
 	return slice[:i] // Return the slice up to index i, which is the new length of the slice
 }
+
+// Ensures that any slice passed to the function contains no duplicate values in the slice
+// Returns a slice that holds all unique values in the original slice
+// Utilizes a pseudo-set by creating a map holding only index values and an empty struct
+// This usage of a map and empty struct reduces memory overhead as struct{}{} takes up no memory
+// and map utilization ensures O(1) lookup speed
+func EnsureUnique[Element comparable](slice []Element) []Element {
+	var set map[Element]struct{} = make(map[Element]struct{}) // Initalizing the map with index generic and empty struct
+	var uniqueElements []Element
+
+	//For every element in the slice
+	for _, element := range slice {
+		//Check if the element exists in the set and if it does not exist
+		if _, exists := set[element]; !exists {
+			//Add element to the set to ensure uniqueness
+			set[element] = struct{}{}
+			//Append the unique elements to the slice that will be returned
+			uniqueElements = append(uniqueElements, element)
+		}
+	}
+	return uniqueElements
+}
